32fc06f4c7492ca43967e4983be68e78c7a5cf5d
kyanon7 <53827693+kyanon7@users.noreply.github.com>
2021-02-04 20:54:41 +0900

    ddddd

--- app.js
+++ app.js
@@ -69,7 +69,7 @@ io.sockets.on('connection', function(socket) {
 })
 
 /* 서버를 8080 포트로 listen */
-server.listen(8080, function() {
+server.listen(3000, function() {
   console.log('서버 실행 중..')
 })
 
